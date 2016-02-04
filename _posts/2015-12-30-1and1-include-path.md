529839feba31f9574f818da3ac5029bff42b0fa1
Gregory McMullen <mcmullen.greg@gmail.com>
2016-02-03 21:24:29 -0500

    Update with nav and blog

---
+++ _posts/2015-12-30-1and1-include-path.md
@@ -0,0 +1,29 @@
+---
+layout: post
+title:  "1and1.com PHP Include Path"
+date:   2015-12-30 15:40:00
+categories: code, php
+---
+
+I've been working on revising the code for [OurMenuMaker.com](http://ourmenumaker.com) and had been 
+struggling with setting the project up properly. 
+
+After looking at different PHP based projects, I determined that I should look into defining my 
+structure similar to the following:
+
+    .
+    ├── public_html
+    |   ├── css
+    |   ├── js
+    |   ├── images
+    |   └── index.php
+    ├── includes
+    |   └── config.php
+
+The end product will be much more sophisticated, but this is why I started researching how to set the include path.
+
+For 1and1.com hosting, the include path needs to be set similarly to:
+
+    include_path = .:/usr/lib/php5.5:/kunden/homepages/XX/XXXXXXXX/htdocs/PATH/TO/YOUR/PROJECT
+
+I modified the `php.ini` file in my project directory and also set the folder/file permissions as appropriate.
--- js/main.js
+++ js/main.js
@@ -24,3 +24,59 @@ jQuery(document).ready(function($){
 		});
 	}
 });
+
+
+// Global Vars
+var wrapper    = $("#site-wrapper"),
+	menu       = $(".menu"),
+	menuLinks  = $(".menu ul li a"),
+	toggle     = $(".nav-toggle"),
+	toggleIcon = $(".nav-toggle span"),
+	sectionID = 1,
+	phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+	twitter = /(^|[^@\w])@(\w{1,15})\b/,
+	email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+
+
+function toggleThatNav() {
+	if (menu.hasClass("show-nav")) {
+		if (!Modernizr.csstransforms) {
+			menu.removeClass("show-nav");
+			toggle.removeClass("show-nav");
+			menu.animate({
+				right: "-=300"
+			}, 500);
+			toggle.animate({
+				right: "-=300"
+			}, 500);
+		} else {
+			menu.removeClass("show-nav");
+			toggle.removeClass("show-nav");
+		}
+
+	} else {
+		if (!Modernizr.csstransforms) {
+			menu.addClass("show-nav");
+			toggle.addClass("show-nav");
+			menu.css("right", "0px");
+			toggle.css("right", "330px");
+		} else {
+			menu.addClass("show-nav");
+			toggle.addClass("show-nav");
+		} 
+	}
+}
+
+function changeToggleClass() {
+	toggleIcon.toggleClass("fa-times");
+	toggleIcon.toggleClass("fa-bars");
+}
+
+$(function() {
+	toggle.on("click", function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		toggleThatNav();
+		changeToggleClass();
+	});
+});
