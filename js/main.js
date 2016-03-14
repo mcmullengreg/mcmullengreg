dddd1be0891cb53c1986f14756032e46c40429c3
Gregory McMullen <mcmullen.greg@gmail.com>
2016-03-14 13:08:58 -0400

    Update js

--- js/main.js
+++ js/main.js
@@ -3,18 +3,3 @@ $('.nav-toggle').click(function(){
 	$(this).toggleClass('open');
 	$(".nav").slideToggle();
 });
-
-function getGit(username) {
-  $.getJSON("https://api.github.com/users/" + username + "/repos?access_token=460652d3e53333c115958f31e88bcd3f300683ca", function(data, status) {
-    var repos = [];
-    $.each(data, function(key, val) {
-      repos.push("<li id=" + val.id + "'><a href='" + val.html_url + "' target='_blank'>" + val.name + "</a></li>");
-    });
-
-    $("<ul/>", {
-      "class": "git-repo",
-      "html": repos.join("")
-    }).appendTo(".repos");
-  }, 'jsonp');
-}
-getGit("mcmullengreg");
