f0de9be500441e8a7c69d6fac9a8586b65c179f1
Gregory McMullen <mcmullen.greg@gmail.com>
2016-03-16 08:42:06 -0400

    Fix hash link issues

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ $('.nav-toggle').click(function(){
 	$(this).toggleClass('open');
 	$(".nav").slideToggle();
 });
+
+if ( window.location.hash ) {
+    var hash = window.location.hash,
+        $hash = $(hash);
+ 
+    $hash.removeAttr('id');
+    $hash.before('<div id="'+hash.slice(1)+'" class="hashlink"></div>');
+    window.location.hash = hash;
+}
